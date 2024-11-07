ed2494901f44015d3bad747d5e6256a09cf5fbf1
Eve C <eve1374@yahoo.com>
2024-11-07 11:26:25 +0100

    commented timer status part of watchfacemeow to go back to a state that compiles

--- src/displayapp/screens/WatchFaceMeow.h
+++ src/displayapp/screens/WatchFaceMeow.h
@@ -7,7 +7,7 @@
 #include <displayapp/Controllers.h>
 #include "displayapp/screens/Screen.h"
 #include "components/datetime/DateTimeController.h"
-#include "displayapp/screens/Timer.h"
+//#include "displayapp/screens/Timer.h"
 #include "utility/DirtyValue.h"
 #include "displayapp/apps/Apps.h"
 
@@ -30,7 +30,7 @@ namespace Pinetime {
                           const Controllers::Battery& batteryController,
                           const Controllers::Ble& bleController,
                           Controllers::AlarmController& alarmController,
-                          Controllers::Timer& timerController,
+                          //Controllers::Timer& timerController,
                           Controllers::NotificationManager& notificationManager,
                           Controllers::Settings& settingsController,
                           Controllers::MotionController& motionController,
@@ -57,7 +57,7 @@ namespace Pinetime {
         Utility::DirtyValue<bool> bleRadioEnabled {};
         bool alarmState {};
         Utility::DirtyValue<std::chrono::time_point<std::chrono::system_clock, std::chrono::minutes>> currentDateTime {};
-        bool timerRunning {};
+        //bool timerRunning {};
         Utility::DirtyValue<uint32_t> stepCount {};
         Utility::DirtyValue<bool> notificationState {};
         Utility::DirtyValue<std::chrono::time_point<std::chrono::system_clock, std::chrono::days>> currentDate;
@@ -79,8 +79,8 @@ namespace Pinetime {
         lv_obj_t* labelAlarm;
         lv_obj_t* labelTimeAmPmAlarm;
         lv_obj_t* alarmIcon;
-        lv_obj_t* labelTimer;
-        lv_obj_t* timerIcon;
+        //lv_obj_t* labelTimer;
+        //lv_obj_t* timerIcon;
         lv_obj_t* pawIcon;
         lv_obj_t* stepValue;
         lv_obj_t* notificationIcon;
@@ -100,7 +100,7 @@ namespace Pinetime {
         const Controllers::Battery& batteryController;
         const Controllers::Ble& bleController;
         Controllers::AlarmController& alarmController;
-        Controllers::Timer& timerController;
+        //Controllers::Timer& timerController;
         Controllers::NotificationManager& notificationManager;
         Controllers::Settings& settingsController;
         Controllers::MotionController& motionController;
@@ -126,7 +126,7 @@ namespace Pinetime {
                                               controllers.batteryController,
                                               controllers.bleController,
                                               controllers.alarmController,
-                                              controllers.timer,
+                                              //controllers.timer,
                                               controllers.notificationManager,
                                               controllers.settingsController,
                                               controllers.motionController,
